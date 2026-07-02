a7450ff4bcf81847cd19ba2e70c4639d39de5c75
gkeras <marcolex30@gmail.com>
2026-07-01 22:41:28 -0500

    first commit

---
+++ python/zigzag/zigzag.py
@@ -0,0 +1,11 @@
+import time, sys
+indent = 0 # How many spaces to indent
+indent_increasing = True # Whether the indentation is increasing or not
+    try:
+        while True: # The main program loop
+            print(' ' * indent, end='')
+            print('********')
+            time.sleep(0.1) # Pause for 1/10th of a second.
+            if indent_increasing
+            # Increase the number of spaces:
+            indent = indent + 1
